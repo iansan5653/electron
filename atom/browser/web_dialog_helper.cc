2050fffe17fe03a3e0464d8e5ec7456a95d00c7f
Shelley Vohr <shelley.vohr@gmail.com>
2019-06-04 16:46:05 -0700

    chore: small memory management improvement

--- atom/browser/web_dialog_helper.cc
+++ atom/browser/web_dialog_helper.cc
@@ -85,10 +85,10 @@ class FileSelectHelper : public base::RefCounted<FileSelectHelper>,
   ~FileSelectHelper() override {}
 
   void EnumerateDirectory(base::FilePath base_dir) {
-    auto* lister = new net::DirectoryLister(
+    lister_.reset(new net::DirectoryLister(
         base_dir, net::DirectoryLister::NO_SORT_RECURSIVE,
-        new atom::DirectoryListerHelper(base_dir, this));
-    lister->Start();
+        new atom::DirectoryListerHelper(base_dir, this)));
+    lister_->Start();
     // It is difficult for callers to know how long to keep a reference to
     // this instance.  We AddRef() here to keep the instance alive after we
     // return to the caller.  Once the directory lister is complete we
@@ -117,9 +117,10 @@ class FileSelectHelper : public base::RefCounted<FileSelectHelper>,
           base_dir = paths[0];
 
           // Actually enumerate soemwhere off-thread
-          base::SequencedTaskRunnerHandle::Get()->PostTask(
-              FROM_HERE, base::BindOnce(&FileSelectHelper::EnumerateDirectory,
-                                        this, base_dir));
+          base::PostTaskWithTraits(
+              FROM_HERE, {content::BrowserThread::IO},
+              base::BindOnce(&FileSelectHelper::EnumerateDirectory, this,
+                             base_dir));
         } else {
           for (auto& path : paths) {
             file_info.push_back(FileChooserFileInfo::NewNativeFile(
@@ -182,8 +183,12 @@ class FileSelectHelper : public base::RefCounted<FileSelectHelper>,
   }
 
   // content::WebContentsObserver:
-  void WebContentsDestroyed() override { render_frame_host_ = nullptr; }
+  void WebContentsDestroyed() override {
+    render_frame_host_ = nullptr;
+    Release();
+  }
 
+  std::unique_ptr<net::DirectoryLister> lister_;
   content::RenderFrameHost* render_frame_host_;
   std::unique_ptr<content::FileSelectListener> listener_;
   blink::mojom::FileChooserParams::Mode mode_;
@@ -275,7 +280,7 @@ void DirectoryListerHelper::OnListFile(
 }
 void DirectoryListerHelper::OnListDone(int error) {
   std::vector<FileChooserFileInfoPtr> file_info;
-  for (auto path : paths_)
+  for (auto& path : paths_)
     file_info.push_back(FileChooserFileInfo::NewNativeFile(
         blink::mojom::NativeFileInfo::New(path, base::string16())));
 
